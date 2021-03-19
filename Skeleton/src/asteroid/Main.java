27fe8578bc5c3505c706c59f623a34a168880099
szabi43 <szabolcsrikli@gmail.com>
2021-03-19 19:27:49 +0100

    frf

--- Skeleton/src/asteroid/Main.java
+++ Skeleton/src/asteroid/Main.java
@@ -1,7 +1,106 @@
 package asteroid;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-	static public void main(String[] args) {
-		System.out.println("Hello world");
+	static public void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Válassz egy tesztesetet! (1-15)");
+		String input = scanner.nextLine();
+		switch(Integer.parseInt(input))
+		{
+			case 1: Teszteset1();
+			case 2: Teszteset2();
+			case 3: Teszteset3();
+			case 4: Teszteset4();
+			case 5: Teszteset5();
+			case 6: Teszteset6();
+			case 7: Teszteset7();
+			case 8: Teszteset8();
+			case 9: Teszteset9();
+			case 10: Teszteset10();
+			case 11: Teszteset11();
+			case 12: Teszteset12();
+			case 13: Teszteset13();
+			case 14: Teszteset14();
+			case 15: Teszteset15();
 		}
+	}
+	
+	private static void Teszteset1()
+	{
+		
+	}
+	
+	private static void Teszteset2()
+	{
+		
+	}
+	
+	private static void Teszteset3()
+	{
+		
+	}
+	
+	private static void Teszteset4()
+	{
+		
+	}
+	
+	private static void Teszteset5()
+	{
+		
+	}
+	
+	private static void Teszteset6()
+	{
+		
+	}
+	
+	private static void Teszteset7()
+	{
+		
+	}
+	
+	private static void Teszteset8()
+	{
+		
+	}
+	
+	private static void Teszteset9()
+	{
+		
+	}
+	
+	private static void Teszteset10()
+	{
+		
+	}
+	
+	private static void Teszteset11()
+	{
+		
+	}
+	
+	private static void Teszteset12()
+	{
+		
+	}
+	
+	private static void Teszteset13()
+	{
+		
+	}
+	
+	private static void Teszteset14()
+	{
+		
+	}
+	
+	private static void Teszteset15()
+	{
+		
+	}
 }

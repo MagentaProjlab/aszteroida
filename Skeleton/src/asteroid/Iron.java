a3bae08b46e9bb99eb9b83241b5c8082912f23e9
Huszti Petra <petrahuszti@gmail.com>
2021-03-19 20:58:40 +0100

    Iron lemaradt

--- Skeleton/src/asteroid/Iron.java
+++ Skeleton/src/asteroid/Iron.java
@@ -15,18 +15,23 @@ package asteroid;
 
 public class Iron extends RawMaterial
 {
+	public Iron() {
+		System.out.println("Ice()");
+	}
 	public void Perish()
 	{
-		
+		System.out.println("Perish()");
+		this.SetAsteroid(null);
 	}
 	
 	public String GetUniqueID()
 	{
+		System.out.println("GetUniqueID()");
 		return "Iron";
 	}
 	
 	public void PerihelionReaction()
 	{
-		
+		System.out.println("PerihelionReaction()");
 	}
 }

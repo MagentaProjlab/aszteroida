3f96b8078eab802c841990fc56508bbe02292c8d
Huszti Petra <petrahuszti@gmail.com>
2021-03-21 22:05:25 +0100

    controller

--- Skeleton/src/asteroid/ControllerClass.java
+++ Skeleton/src/asteroid/ControllerClass.java
@@ -10,7 +10,9 @@ public class ControllerClass
 	private ArrayList<Robot> robots;
 	private SolarWind solarwind;
 	private ArrayList<BillOfMaterials> bills;
-	
+	/**
+	 * A controller konstruktora
+	 */
 	public ControllerClass() {
 		Logger.MethodCall("ControllerClass()");
 		asteroids = new ArrayList<Asteroid>();
@@ -22,13 +24,17 @@ public class ControllerClass
 		InitBills();
 		Logger.MethodReturn("");
 	}
-	
+	/**
+	 * A controller jatekciklusa
+	 */
 	public void GameLoop()
 	{
 		Logger.MethodCall("GameLoop()");
 		Logger.MethodReturn("void");
 	}
-	
+	/**
+	 * A controller inicializalja az aszteroidakat
+	 */
 	public void InitAsteroids()
 	{
 		Logger.MethodCall("InitAsteroids()");
@@ -36,7 +42,9 @@ public class ControllerClass
 		ConnectAsteroids();
 		Logger.MethodReturn("void");
 	}
-	
+	/**
+	 * A controller inicializalja a telepeseket
+	 */
 	public void InitSettlers()
 	{
 		Logger.MethodCall("InitSettlers()");
@@ -45,19 +53,26 @@ public class ControllerClass
 		s1.setAsteroid(a1);
 		Logger.MethodReturn("void");
 	}
-	
+	/**
+	 * A controller inicializalja a billeket
+	 */
 	public void InitBills() 
 	{
 		Logger.MethodCall("InitBills()");
 		Logger.MethodReturn("void");
 	}
-	
+	/**
+	 * A controller osszekoti az aszteroidakat
+	 */
 	public void ConnectAsteroids() 
 	{
 		Logger.MethodCall("ConnectAsteroids()");
 		Logger.MethodReturn("void");
 	}
-	
+	/**
+	 * A controller visszaad egy random aszteroidat
+	 * @return asteroid : a visszaadando aszteroida - egyenlore null - t ad vissza
+	 */
 	public Asteroid GetRandomAsteroid() 
 	{
 		Logger.MethodCall("GetRandomAsteroid()");

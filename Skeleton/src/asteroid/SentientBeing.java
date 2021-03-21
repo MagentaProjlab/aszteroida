6b98a77f60ecc2c104281ffa01b876d9f364b65a
Huszti Petra <petrahuszti@gmail.com>
2021-03-21 21:50:33 +0100

    komm

--- Skeleton/src/asteroid/SentientBeing.java
+++ Skeleton/src/asteroid/SentientBeing.java
@@ -3,17 +3,35 @@ package asteroid;
 public abstract class SentientBeing
 {
 	protected Asteroid location;
-	
+	/**
+	 * A sentientbeing beallitja az aszteroidajat
+	 * @param loc : A beallitando aszteroida
+	 */
 	public void setAsteroid(Asteroid loc)
 	{
 		Logger.MethodCall("setAsteroid(Asteroid loc)");
 		this.location = loc;
 		Logger.MethodReturn("void");
 	}
-	
+	/**
+	 * A leny absztrakt mozogasert felelos fuggvenye
+	 * @param place: A hely ahova mozog
+	 */
 	public abstract void Move(Place place);
+	/**
+	 * A leny absztrakt furasert felelos fuggvenye
+	 */
 	public abstract void Drill();
+	/**
+	 * A leny absztrakt halalert felelos fuggvenye
+	 */
 	public abstract void Die();
+	/**
+	 * A leny absztrakt felrobbanaert felelos fuggvenye
+	 */
 	public abstract void Explode();
+	/**
+	 * A leny absztrakt lepesert felelos fuggvenye
+	 */
 	public abstract void Step();
 }

de6832d04de4dc7a8c860a10176155e60d05299f
1kalacs <79051687+1kalacs@users.noreply.github.com>
2021-04-19 11:01:52 +0200

    listbeings fullname... de neekm valamiert egy sorba kerul az output

--- Prototipus/src/asteroid/ControllerClass.java
+++ Prototipus/src/asteroid/ControllerClass.java
@@ -234,7 +234,7 @@ public class ControllerClass
 				{
 					for(SentientBeing sb : a.getBeings())
 					{
-						String message = "[SentientBeing: " + sb.getName() + "]";
+						String message = sb.getFullName();
 						Logger.Message(message);
 					}
 					break;
